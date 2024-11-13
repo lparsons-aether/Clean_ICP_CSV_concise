09a1c5c781c423fa222760763dbbdda01e94fb63
lparsons-aether <167369581+lparsons-aether@users.noreply.github.com>
2024-11-13 08:33:41 -0800

    Add files via upload

---
+++ Clean_ICP_CSV_concise/clean_multiple.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import os
+
+from write_clean_csv import write_clean_csv
+from reorganize_headers import reorganize_clean_headers
+
+# Make the directories for the outputs. You can change these but good to keep them the same
+clean_icp_dir = "/Users/leoparsons/Desktop/Aether_Biomachines/Experiments/ICP-OES/IX cycling clean concise icp reports"
+if not os.path.exists(clean_icp_dir):
+    os.mkdir(clean_icp_dir)
+
+reorganized_clean_icp_dir = "/Users/leoparsons/Desktop/Aether_Biomachines/Experiments/ICP-OES/IX cycling reorganized " \
+                            "clean concise icp reports"
+if not os.path.exists(reorganized_clean_icp_dir):
+    os.mkdir(reorganized_clean_icp_dir)
+
+def loop_through_folder(directory_str):
+    """loops through a folder and returns a list of file paths as strings for all items in the folder"""
+    directory = os.fsencode(directory_str)
+    master_list = []
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename == ".DS_Store":
+            pass
+        elif ".csv" in filename:
+            file_path = directory_str + "/" + filename
+            master_list.append(file_path)
+        else:
+            pass
+
+    return sorted(master_list)
+
+
+list_icp_concise_path = loop_through_folder("/Users/leoparsons/Desktop/Aether_Biomachines/"
+                                          "Experiments/ICP-OES/IX cycling concise icp reports")
+
+for path in list_icp_concise_path:
+    output_dir = clean_icp_dir
+    clean_icp_filepath = write_clean_csv(filepath_in=path, filepath_out=output_dir)
+
+list_icp_clean_concise_path = loop_through_folder(clean_icp_dir)
+
+for path in list_icp_clean_concise_path:
+    output_dir = reorganized_clean_icp_dir
+    reorganized_clean_icp_filepath = reorganize_clean_headers(filepath_in=path, filepath_out=output_dir)
+
+def combine_df(path_list: list):
+    """takes a list of paths to csv files you want to concat"""
+    # Turn csv into pandas dfs
+    df_list = []
+    for path in path_list:
+        df = pd.read_csv(path)
+        df_list.append(df)
+    concat_df = pd.concat(df_list, ignore_index=True)
+
+    return concat_df
+
+
+# This will be the running ICP DB file which you can upload stuff to
+ICP_DB_filename = "/Users/leoparsons/Desktop/Aether_Biomachines/Experiments/ICP-OES/ICP DB.csv"
+combine_df(loop_through_folder(reorganized_clean_icp_dir)).set_index("sample_name").to_csv(ICP_DB_filename)
---
+++ Clean_ICP_CSV_concise/csv_organizer_concise.py
@@ -0,0 +1,169 @@
+import csv
+from itertools import groupby
+import pandas as pd
+
+
+def clean_line(line):
+    return line.replace('"', "").strip()
+
+
+def all_equal(iterable):
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)
+
+def read_local_icp_file(icp_file):
+    results = {}
+    with open(icp_file) as f:
+        reader = csv.reader(f)
+        line = next(reader)
+        line = line[:-1]
+        while not (line[0] == '' and all_equal(line)):
+            # currently looking at the run method data
+            if 'Session Summary' not in line[0]:
+                raise Exception("Missing Session Summary")
+            line = next(reader)
+            results['session_summary'] = {}
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                key = line[0].split(":")[0]
+                if len(line[0].split(":")) > 1:
+                    value = line[0].split(":")[1]
+                else:
+                    value = ""
+                results['session_summary'][key] = value
+                line = next(reader)
+            line = next(reader)
+
+            if 'Method Summary' not in line[0]:
+                raise Exception("Missing Method Summary")
+            results['method_summary'] = {}
+            line = next(reader)
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                key = line[0].split(":")[0]
+                if len(line[0].split(":")) > 1:
+                    value = line[0].split(":")[1]
+                else:
+                    value = ""
+                results['method_summary'][key] = value
+                line = next(reader)
+            line = next(reader)
+
+            if 'Instrument ID' not in line:
+                raise Exception("Missing Instrument ID")
+            results['instrument_id'] = {}
+            line = next(reader)
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                key = line[0].split(":")[0]
+                if len(line[0].split(":")) > 1:
+                    value = line[0].split(":")[1]
+                else:
+                    value = ""
+                results['instrument_id'][key] = value
+                line = next(reader)
+            line = next(reader)  # "Results corrected by IS"
+            if "Results corrected by IS" in line[0]:
+                results['is_correction'] = True
+            else:
+                results['is_correction'] = False
+                # raise NotImplementedError("Not ready to handle no is correction")
+            line = next(reader)  # blank line before measurements
+
+            results['measurement_list'] = []
+            line = next(reader)
+
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                measurement = {}
+                # line = clean_line(line)
+                line_split = line
+                name = line_split.pop(0)
+                measurement['name'] = name
+                if name == "Method Instrument and Sampling Parameters":
+                    line.insert(0, name)
+                    break
+                for str in line_split:
+                    if '[' in str and ']' in str:
+                        unit = str[str.find('[') + 1: str.find(']')]  # split out unit
+                        str = str[:str.find('[') + 1] + str[str.find(']') + 1:]  # remove unit from key
+                    key = '_'.join(str.lower().split(':')[0].split(' '))
+                    value = ''
+                    if len(str.lower().split(':')) > 1:
+                        value = str.lower().split(':')[1].strip(",").strip()
+                    measurement[key] = value
+                line = next(reader)
+                column_list = line
+                # hard code here for first column
+                column_list[0] = 'ion_wavelength_unit_replicate'
+                measurement['value_list'] = []
+                line = next(reader)
+
+                while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                    measurement_value = {}
+                    val_list = line
+                    val_list = val_list + ([''] * (len(column_list) - len(val_list)))
+                    for index, key in enumerate(column_list):
+                        measurement_value[key] = val_list[index]
+                    measurement['value_list'].append(measurement_value)
+                    line = next(reader)
+                results['measurement_list'].append(measurement)
+                line = next(reader)
+
+            # line = next(reader)  # move to method instrument and sampling parameters
+
+            if 'Method Instrument and Sampling Parameters' not in line[0]:
+                raise Exception("Method Instrument and Sampling Parameters not in file")
+            line = next(reader)
+            results['method_parameters'] = {}
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                val_list = line
+                for line_val in val_list:
+                    if '[' in line_val and ']' in line_val:
+                        unit = line_val[line_val.find('[') + 1: line_val.find(']')]  # split out unit
+                        line_val = line_val[:line_val.find('[')] + line_val[
+                                                                   line_val.find(']') + 1:]  # remove unit from key
+                        line_val = line_val.strip()
+                        key = '_'.join(line_val.lower().split(':')[0].split(' '))
+                        results['method_parameters'][key + '_unit'] = unit.strip()
+                    key = '_'.join(line_val.lower().split(':')[0].split(' '))
+                    value = ''
+                    if len(line[0].lower().split(':')) > 1:
+                        value = ':'.join(line_val.lower().split(':')[1:]).strip()
+                results['method_parameters'][key] = value.strip()
+                line = next(reader)
+            line = next(reader)
+
+            if 'Method Elements' not in line[0]:
+                raise Exception("Method Elements not in file")
+            line = next(reader)
+            results['method_elements'] = {}
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                key = '_'.join(line[0].lower().split('-')[0].split(' '))
+                value_list = line[0].lower().split('-')[1].split(',')
+                results['method_elements'][key] = []
+                for value in value_list:
+                    if value == '':
+                        continue
+                    results['method_elements'][key].append(float(value))
+                line = next(reader)
+            line = next(reader)
+
+            if 'Method Standards' not in line[0]:
+                raise Exception("Method Standards not in file")
+            results['method_standards'] = {}
+            line = next(reader)
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                key = line[0]
+                value_list = line.pop(0)
+                results['method_standards'][key] = value_list
+                line = next(reader)
+            line = next(reader)
+
+            if "Internal Standards" not in line[0]:
+                raise Exception("Internal Standards not in file")
+            results['method_internal_standards'] = {}
+            line = next(reader)
+            while not (len(line) == 0 or (line[0] == '' and all_equal(line))):
+                key = line[0].split(" -")[0]
+                value_list = key + line[0].split(",")[1]
+                results['method_internal_standards'][key] = value_list
+                line = next(reader)
+
+            return results
