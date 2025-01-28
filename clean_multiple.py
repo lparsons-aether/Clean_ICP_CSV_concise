7a47828ce3d445b35cf04bd1ef6050ea79206728
Leo Parsons <leoparsons@MacBook-Pro-240.local>
2025-01-28 13:58:49 -0800

    output directories are now based on the directory selected by the tkinter prompt

--- clean_multiple.py
+++ clean_multiple.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import numpy as np
 import os
+import tkinter as tk
+from tkinter import filedialog
 
 from write_clean_csv import write_clean_csv
 from reorganize_headers import reorganize_clean_headers
 
+root = tk.Tk()
+root.withdraw()
+print('select folder containing concise reports')
+
+file_path = filedialog.askdirectory()  # can change how filepath is accessed as needed
+file_dir = '/'.join(file_path.split('/')[:-1])
+
+print(f'Selected filepath: {file_path}')
+
 # Make the directories for the outputs. You can change these but good to keep them the same
 
-clean_icp_dir = "/Users/leoparsons/Desktop/Aether_Biomachines/Experiments/ICP-OES/IX cycling clean concise icp reports"
+clean_icp_dir = file_dir + "/IX cycling clean concise icp reports"
 if not os.path.exists(clean_icp_dir):
     os.mkdir(clean_icp_dir)
 
-reorganized_clean_icp_dir = "/Users/leoparsons/Desktop/Aether_Biomachines/Experiments/ICP-OES/IX cycling reorganized " \
-                            "clean concise icp reports"
+reorganized_clean_icp_dir = file_dir + "/IX cycling reorganized clean concise icp reports"
 if not os.path.exists(reorganized_clean_icp_dir):
     os.mkdir(reorganized_clean_icp_dir)
 
@@ -33,13 +43,15 @@ def loop_through_folder(directory_str):
     return sorted(master_list)
 
 
-list_icp_concise_path = loop_through_folder("/Users/leoparsons/Desktop/Aether_Biomachines/"
-                                          "Experiments/ICP-OES/IX cycling concise icp reports")
+list_icp_concise_path = loop_through_folder(file_path)
+
 
 print("generating concise reports...")
 for path in list_icp_concise_path:
     output_dir = clean_icp_dir
     clean_icp_filepath = write_clean_csv(filepath_in=path, filepath_out=output_dir)
+
+print(f'concise reports generated: {clean_icp_dir}')
 
 list_icp_clean_concise_path = loop_through_folder(clean_icp_dir)
 
@@ -47,6 +59,8 @@ print("generating clean concice reports...")
 for path in list_icp_clean_concise_path:
     output_dir = reorganized_clean_icp_dir
     reorganized_clean_icp_filepath = reorganize_clean_headers(filepath_in=path, filepath_out=output_dir)
+
+print(f"clean concise reports generated: {reorganized_clean_icp_dir}")
 
 def combine_df(path_list: list):
     """takes a list of paths to csv files you want to concat"""
